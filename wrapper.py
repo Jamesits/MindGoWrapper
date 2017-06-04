2b4be66bd69204f29e3714fc51006fb955d6a16a
James Swineson <github@public.swineson.me>
2017-06-04 14:42:17 +0800

    update test code

---
+++ wrapper.py
@@ -0,0 +1,42 @@
+from scheduler import Scheduler
+import logging
+
+class Wrapper():
+  def _mindgo_initialize(self, account):
+    self.account = account
+    self.log.debug('_mindgo_initialize')
+
+  def _mindgo_handle_data(self, account, data):
+    self.account = account
+    self.data = data
+    self.log.debug('_mindgo_handle_data')
+
+  def _mindgo_before_trading_start(self, account, data):
+    self.account = account
+    self.data = data
+    self.log.debug('_mindgo_before_trading_start')
+
+  def _mindgo_after_trading_end(self, account, data):
+    self.account = account
+    self.data = data
+    self.log.debug('_mindgo_after_trading_end')
+
+  def __init__(self):
+    # self.scheduler = Scheduler()
+    # MindGo global objects
+    self.account = None
+    self.data = None
+    self.log = logging.getLogger("MindGoWrapper")
+    self.log.setLevel(logging.DEBUG)
+
+  def takeown(self):
+    print('MindGoWrapper {}\nhttps://github.com/Jamesits/MindGoWrapper'.format(__version__))
+    # takeown all MindGo callback functions
+    global initialize
+    global handle_data
+    global before_trading_start
+    global after_trading_end
+    initialize = self._mindgo_initialize
+    handle_data = self._mindgo_handle_data
+    before_trading_start = self.before_trading_start
+    after_trading_end = self.after_trading_end
