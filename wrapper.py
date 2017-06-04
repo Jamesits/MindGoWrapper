3b73c7e91ac3d5cdd4525ead6c9954079b11e958
James Swineson <github@public.swineson.me>
2017-06-04 14:53:59 +0800

    remove version print

--- wrapper.py
+++ wrapper.py
@@ -30,7 +30,7 @@ class Wrapper():
     self.log.setLevel(logging.DEBUG)
 
   def takeown(self):
-    print('MindGoWrapper {}\nhttps://github.com/Jamesits/MindGoWrapper'.format(__version__))
+    print('MindGoWrapper\nhttps://github.com/Jamesits/MindGoWrapper')
     # takeown all MindGo callback functions
     global initialize
     global handle_data
