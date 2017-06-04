8d637307e7a54e1902f7623a2f84e7b6661d9a2e
James Swineson <github@public.swineson.me>
2017-06-04 18:58:20 +0800

    update comment

--- scheduler.py
+++ scheduler.py
@@ -15,8 +15,8 @@ class Scheduler():
     @staticmethod
     def timeslot(position, length, unit, slot):
         '''创建一个待办事项
-        position：在第 N 个单位时间处触发
-        length：每隔几个单位时间重复发生
+        position：在第 N 个单位时间处触发（0<=N<length）
+        length：循环周期
         unit：时间单位
         slot：发生在该时间的系统事件之前还是之后'''
         ret = []
