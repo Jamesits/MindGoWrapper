2d51703a8b4a569617773213f333a14f1869bd11
James Swineson <github@public.swineson.me>
2017-06-04 15:02:11 +0800

    everything use info

--- wrapper.py
+++ wrapper.py
@@ -4,22 +4,22 @@ import logging
 class Wrapper():
   def _mindgo_initialize(self, account):
     self.account = account
-    self.log.debug('_mindgo_initialize')
+    self.log.info('_mindgo_initialize')
 
   def _mindgo_handle_data(self, account, data):
     self.account = account
     self.data = data
-    self.log.debug('_mindgo_handle_data')
+    self.log.info('_mindgo_handle_data')
 
   def _mindgo_before_trading_start(self, account, data):
     self.account = account
     self.data = data
-    self.log.debug('_mindgo_before_trading_start')
+    self.log.info('_mindgo_before_trading_start')
 
   def _mindgo_after_trading_end(self, account, data):
     self.account = account
     self.data = data
-    self.log.debug('_mindgo_after_trading_end')
+    self.log.info('_mindgo_after_trading_end')
 
   def __init__(self):
     # self.scheduler = Scheduler()
@@ -27,7 +27,7 @@ class Wrapper():
     self.account = None
     self.data = None
     self.log = logging.getLogger("MindGoWrapper")
-    self.log.setLevel(logging.DEBUG)
+    self.log.setLevel(logging.INFO) # seems of no use
 
   def takeown(self):
     self.log.info('MindGoWrapper\nhttps://github.com/Jamesits/MindGoWrapper')
