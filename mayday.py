2dde80e86bd5f7ee7467047334cb29d0c6cad2e9
James Swineson <github@public.swineson.me>
2017-06-06 14:41:18 +0800

    Change output format

--- mayday.py
+++ mayday.py
@@ -5,6 +5,7 @@
 
 import sys
 import logging
+import traceback
 
 
 class Mayday():
@@ -17,19 +18,15 @@ class Mayday():
         # Construct log string
         logstr = '''%%% Unhandled exception %%%
         Date: {}, Days = {}, Ticks = {}
-        Exception: {} {}
-        Traceback: 
         {}
         '''.format(
             self.wrapper.date.strftime("%Y-%m-%d"),
             self.wrapper.days,
             self.wrapper.ticks,
-            e_type,
-            e_value,
-            e_traceback,
+            traceback.format_exception((e_type, e_value, e_traceback))
         )
         # Output twice using logging module and platform's log function
-        self.log.error(logstr)
+        self.log.critical(logstr)
         self.wrapper.platform_apis.log.info(logstr)
         # Run the original excepthook
         sys.__excepthook__(e_type, e_value, e_traceback)
@@ -38,3 +35,4 @@ class Mayday():
         sys.excepthook = self.__excepthook
         self.wrapper = wrapper
         self.log = logging.getLogger("MindGoWrapper.Mayday")
+        self.log.info("MindGoWrapper Mayday initialized")
