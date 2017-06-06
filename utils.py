d1349e7a9e6c18284210e0ebb75a7554e2d7339c
James Swineson <github@public.swineson.me>
2017-06-06 16:27:03 +0800

    增加一个识别当前运行环境（回测还是 Jupyter）的函数

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@
 # by James Swineson, 2017-06
 # https://github.com/Jamesits/MindGoWrapper
 
+import sys
+
 
 def df_iter(df, seq, rank=False, ascending=True):
     '''遍历 dataframe 的每个下标，可选使用数值或者排名'''
@@ -13,3 +15,17 @@ def df_iter(df, seq, rank=False, ascending=True):
             yield df['rank_' + i]
         else:
             yield df[i]
+
+
+def detect_runtime():
+    '''识别当前在哪种运行环境下
+    如果在“我的策略”当中回测，返回 'strategy'
+    如果在“我的研究”中的 Jupyter 环境里回测，返回 'research'
+    无法识别则返回 'unknown'
+    '''
+    if 'ipykernel' in sys.modules:
+        return 'research'
+    elif 'IPython' in sys.modules:
+        return 'strategy'
+    else:
+        return 'unknown'
