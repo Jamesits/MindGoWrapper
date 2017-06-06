956d7135ba86d935124e32640a8f94a4d4fca19e
James Swineson <github@public.swineson.me>
2017-06-06 14:54:41 +0800

    Catch all exceptions in mayday before backtest

--- mayday.py
+++ mayday.py
@@ -12,24 +12,28 @@ class Mayday():
     '''MindGoWrapper 的自定义未处理异常输出'''
 
     def __excepthook(self, e_type, e_value, e_traceback):
+        '''Runs when there is a global uncatched exception'''
         if issubclass(e_type, KeyboardInterrupt):
             sys.__excepthook__(e_type, e_value, e_traceback)
             return
+        self.log_exception((e_type, e_value, e_traceback))
+        sys.__excepthook__(e_type, e_value, e_traceback)
+
+    def log_exception(self, additional_message, exc_info):
         # Construct log string
         logstr = '''%%% Unhandled exception %%%
-        Date: {}, Days = {}, Ticks = {}
+        Date: {}, Days = {}, Ticks = {}, Additional message: {}
         {}
         '''.format(
             self.wrapper.date.strftime("%Y-%m-%d"),
             self.wrapper.days,
             self.wrapper.ticks,
-            traceback.format_exception((e_type, e_value, e_traceback))
+            additional_message,
+            traceback.format_exception(exc_info)
         )
         # Output twice using logging module and platform's log function
         self.log.critical(logstr)
         self.wrapper.platform_apis.log.info(logstr)
-        # Run the original excepthook
-        sys.__excepthook__(e_type, e_value, e_traceback)
 
     def __init__(self, wrapper):
         sys.excepthook = self.__excepthook
