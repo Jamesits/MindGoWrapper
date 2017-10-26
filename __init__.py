90cdca961aa3ddf15ac4ff6818aa95b61a3df681
James Swineson <github@public.swineson.me>
2017-10-26 10:05:31 +0800

    Add __name__ to __init__.py to workaround '__name__' not in globals exception

--- __init__.py
+++ __init__.py
@@ -3,5 +3,9 @@
 # by James Swineson, 2017-06
 # https://github.com/Jamesits/MindGoWrapper
 
-all = ['map', 'mayday', 'moduleproxy', 'portfolio', 'scheduler', 'utils', 'wrapper']
+
+__name__ = 'MindGoWrapper'
+__package__ = 'MindGoWrapper'
 __version__ = '0.0.2'
+
+all = ['map', 'mayday', 'moduleproxy', 'portfolio', 'scheduler', 'utils', 'wrapper']
