048794e891cc1d6195e25db09d5a649e3efacb97
James Swineson <github@public.swineson.me>
2017-10-26 09:13:00 +0800

    import multiple modules at a time

--- moduleproxy.py
+++ moduleproxy.py
@@ -1,8 +1,15 @@
 import __future__
+import collections
 
 class ModuleProxy():
     def __init__(self):
         self.proxied_modules = {}
 
-    def import_module(self, name: str) -> object:
-        self.proxied_modules[name] = __future__.__builtins__["__import__"](name)
+    def import_module(self, name: object) -> object:
+        if isinstance(name, str):
+            self.proxied_modules[name] = __future__.__builtins__["__import__"](name)
+        elif isinstance(name, collections.Iterable): # assuming list or tuples here
+            for item in name:
+                self.import_module(item)
+        else:
+            raise ValueError("name should be string or a list/tuple of strings")
